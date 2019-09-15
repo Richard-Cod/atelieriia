88a786357c571054170356678ada714754fab2c6
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-09-15 07:54:19 +0000

    add heroku remote

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn atelieriia.wsgi --log-file -
---
+++ atelieriia/prod_settings.py
@@ -0,0 +1,11 @@
+from portfolio.settings import *
+import dj_database_url
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+ALLOWED_HOST = ['atelieriia.herokuapp.com']

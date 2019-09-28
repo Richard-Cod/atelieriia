7765514d447cb5bbc0f2f1558e1a78620efd9483
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-09-28 19:28:06 +0000

    pause fixing

--- atelieriia/settings.py
+++ atelieriia/settings.py
@@ -137,12 +137,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-MEDIA_URL = '/media/'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),)
     
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+
+

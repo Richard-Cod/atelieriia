5686cb8c1a1c09bf62950dd222c8bcd0ae0947bd
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-11-23 10:32:24 +0000

    fixing projects with new domain name

--- projets/mock_projets.py
+++ projets/mock_projets.py
@@ -4,9 +4,9 @@ projets_data =[
 			'titre':'HTML CSS',
 			'description':'Vous avez finis le cours de html css il est temps de passer a la pratique avec un projet ',
 			'cours':'HTMLCSS',
-			'lien_vue':'http://www.voyage.hermann.compteweb.com/',
-			'image':'',
-			'lien_image':'http://www.hermann.compteweb.com/images/voyage.png',
+			'lien_vue':'https://atelieriia-projets.000webhostapp.com/Projet_HTMLCSS/',
+			'image':'htmlcss.jpg',
+			'lien_image':'',
 			
 			},
 			{
@@ -14,12 +14,21 @@ projets_data =[
 			'titre':'Bootstrap',
 			'description':'Vous avez finis le cours de Bootstrap il est temps de passer a la pratique avec un projet ',
 			'cours':'BOOTSTRAP',
-			'lien_vue':'http://www.hermann.compteweb.com/',
+			'lien_vue':'https://atelieriia-projets.000webhostapp.com/projet_BOOTSTRAP/',
 			'image':'bootstrap.jpg',
 			'lien_image':'',
 			
 			},
-			{
+			
+
+
+	
+		
+		
+	]
+
+"""
+{
 			'id':2,
 			'titre':'PHP & MySql',
 			'description':'Vous avez finis le cours de php il est temps de passer a la pratique avec un projet ',
@@ -30,8 +39,4 @@ projets_data =[
 			
 			},
 
-
-	
-		
-		
-	]
+"""

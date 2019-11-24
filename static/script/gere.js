8fbbd19ab07e2fbbabfbc6043ec4ca9355682570
Richard-Cod <richard.bathiebo.7@gmail.com>
2019-11-24 08:31:29 +0000

    clean

--- static/script/gere.js
+++
@@ -1,9 +0,0 @@
-/*
-
-import Swup from 'swup';
-const swup = new Swup(); // only this line when included with script tag
-
-
-console.log("DANS LE MODULE")
-
-*/
--- static/script/script.js
+++
@@ -1,8 +0,0 @@
-function write() {
-	// body...
-	inputs = document.getElementsByTagName('input')
-	for (var i = 0; i < inputs.length; i++) {
-		inputs[i].value = inputs[i].name
-	}
-}
-
